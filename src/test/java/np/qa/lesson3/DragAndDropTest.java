0aa574d96877f44d13687ae910e901caf589e138
Mark Mark <antiutka@gmail.com>
2021-12-24 08:59:51 +0300

    1 задание 2 задание убраны коллекции

--- src/test/java/np/qa/lesson3/DragAndDropTest.java
+++ src/test/java/np/qa/lesson3/DragAndDropTest.java
@@ -5,10 +5,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Configuration.baseUrl;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class DragAndDropTest {
     @BeforeAll
--- src/test/java/np/qa/lesson3/SelenideWikiTest.java
+++ src/test/java/np/qa/lesson3/SelenideWikiTest.java
@@ -4,13 +4,12 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Configuration.baseUrl;
-import static com.codeborne.selenide.Selectors.*;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class SelenideWikiTest {
     @BeforeAll
@@ -18,21 +17,34 @@ public class SelenideWikiTest {
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = false;
         baseUrl = "https://github.com";
+
     }
 
-    // 1) разница есть: $("h1 div") - поиск по всему дереву,  $("h1").$("div") - только в первом уровне
+    /*
+    1)
+
+    $("h1 div") - найдет, тк во втором h1 есть div.
+    $("h1").$("div") - не найдет, тк только смотрит на первый h1
+
+    <h1> <ol>
+        <\ol> <\h1>
+
+    <h1> <div>
+        <\div> <\h1>
+
+    */
 
 
     @Test
     void selenideWikiTest() {
         open(baseUrl);
         $("[data-test-selector=nav-search-input]").setValue("Selenide").pressEnter();
-        $$("ul.repo-list li").first().$("a").click();
+        $("ul.repo-list li").$("a").click();
         $("h1").shouldHave(text("Selenide / Selenide"));
         $("#wiki-tab").click();
-        $$("#wiki-pages-box").first().$("li.Box-row.wiki-more-pages-link").$("button").click();
-        $$("#wiki-pages-box").first().shouldHave(text("SoftAssertions")).shouldBe(visible);
-        $$("#wiki-pages-box").first().find(byText("SoftAssertions")).click();
+        $("#wiki-pages-box").$("li.Box-row.wiki-more-pages-link").$("button").click();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions")).shouldBe(visible);
+        $("#wiki-pages-box").find(byText("SoftAssertions")).click();
         $("#wiki-body").shouldHave(text("Using JUnit5"));
     }
 
